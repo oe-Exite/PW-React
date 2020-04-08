f4bb75f2291f191e5988fe4d0cbb5d5173c14ca3
Alexander Frolov <frolov@sharp-dev.net>
2020-04-08 18:53:55 +0500

    added loading indicator

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -5,57 +5,46 @@ import UserService from '../../services/user-service';
 import TransactionsTable from '../transactions-list/transactions-list';
 import TransactionForm from '../transaction-form/transaction-form';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './profile.module.scss';
-
-const testTransactions = [
-    {id: 1, username: 'user 1', date: Date.now(), amount: -12, balance: 500},
-    {id: 2, username: 'user 2', date: Date.now(), amount: 12, balance: 500},
-    {id: 3, username: 'user 3', date: Date.now(), amount: -12, balance: 500},
-    {id: 4, username: 'user 4', date: Date.now(), amount: 12, balance: 500},
-    {id: 5, username: 'user 5', date: Date.now(), amount: -12, balance: 500},
-    {id: 6, username: 'user 6', date: Date.now(), amount: 12, balance: 500},
-    {id: 7, username: 'user 7', date: Date.now(), amount: -12, balance: 500},
-    {id: 8, username: 'user 8', date: Date.now(), amount: -12, balance: 500},
-    {id: 9, username: 'user 9', date: Date.now(), amount: -12, balance: 500},
-    {id: 10, username: 'user 10', date: Date.now(), amount: -12, balance: 500},
-  ];
-
 
 class Profile extends Component {
     static contextType = StateContext;
 
     state = {
-        selectedTransaction: null
+        selectedTransaction: null,
+        isLoaded: false
     }
 
     componentDidMount() {
         const [{}, dispatch] = this.context;
-        UserService.getUserinfo(dispatch).then((res) => {
-            UserService.getUserTransactions(dispatch);
-        })
-        .catch((error) => {
-            console.log('userRequest error:', error);
-            this.props.history.push('/login');
+        UserService.getUserinfo(dispatch).then((res) =>
+            UserService.getUserTransactions(dispatch)
+        )
+        .catch((error) =>
+            this.props.history.push('/login')
+        )
+        .finally(() => {
+            this.setState({isLoaded: true})
         });
-        //.finally(() => this.setState({isLoaded: true}));
     }
 
     handleTransactionSelect = (transaction) => {
         this.setState({
-            selectedTransaction: transaction
+            selectedTransaction: transaction ? { ...transaction, amount: Math.abs(transaction.amount) } : null
         });
     }
 
     newTransaction = () => {
         this.setState({
-            selectedTransaction: {}
+            selectedTransaction: { username: '', amount: 1 }
         });
     }
 
     render() {
         const [{ user, transactions }, dispatch] = this.context;
-        if (!user) {
-            return null;
+        if (!this.state.isLoaded) {
+            return <CircularProgress />;
         } else {
             return (
                 <div className={styles.profile}>
@@ -74,7 +63,7 @@ class Profile extends Component {
                             <h3 className={styles.profile__notransactions}>You don't have transactions yet</h3>
                          }
                     </div>
-                    {this.state.selectedTransaction &&
+                    { this.state.selectedTransaction &&
                         <TransactionForm className={styles.profile__part}
                             selectedTransaction={this.state.selectedTransaction} onTransactionClose={this.handleTransactionSelect}/>
                     }
